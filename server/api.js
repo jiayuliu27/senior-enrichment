88e5f23555aef1d1d20ca47651321a9cd444cda1
jiayuliu27 <jiayuliu@umich.edu>
2016-11-22 21:11:59 -0500

    revert

--- server/api.js
+++ server/api.js
@@ -4,4 +4,8 @@ const db = require('APP/db')
 
 api.get('/hello', (req, res) => res.send({hello: 'world'}))
 
+	.use('/users', require('./users'))
+	.use('/questions', require('./questions'))
+	.use('/comments', require('./comments'))
+
 module.exports = api
