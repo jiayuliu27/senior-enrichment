5646606df19314ca7e8ed8298285b12dff9f0b37
jiayuliu27 <jiayuliu@umich.edu>
2016-11-22 21:00:03 -0500

    update models and routes

--- db/models/user.js
+++ db/models/user.js
@@ -3,5 +3,19 @@ var Sequelize = require('sequelize')
 var db = require('APP/db')
 
 module.exports = db.define('users', {
-  name: Sequelize.STRING,
-})
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: {
+  	type: Sequelize.STRING,
+  	allowNull: false,
+  	validate: {
+  		isEmail: true
+  	}
+  }
+}, {
+	getterMethods: {
+		fullName: () => {
+			return this.firstName + ' ' + this.lastName;
+		}
+	}
+});
